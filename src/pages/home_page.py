ea2617346aefd0d2df5e9694815ba56a401b0e7b
sasha.enko <sasha.enko.ua@gmail.com>
2025-08-26 03:05:13 +0300

    PANEL if made a class attr instead of instance attr

--- src/pages/home_page.py
+++ src/pages/home_page.py
@@ -1,9 +1,9 @@
 # src/pages/home_page.py
 from playwright.sync_api import Page
-from src.pages.base_page import BasePage
 from src.urls_resolver import get_url_for
 
-from home_page_selectors import (
+from src.pages.base_page import BasePage
+from src.pages.home_page_selectors import (
     HomePageSelectors as SelectorFor,
     HomePagePanelNames as PanelName
 )
@@ -12,14 +12,13 @@ from home_page_selectors import (
 class HomePageSync(BasePage):
     HOME_URL = get_url_for('home')
 
+    PANELS = {
+        panel.value: f'{SelectorFor.PANEL_DIV.value}:has-text("{panel.value}")'
+        for panel in PanelName
+    }
 
     def __init__(self, page: Page):
         super().__init__(page, self.HOME_URL, SelectorFor.UNIQUE_PAGE_DEFINER.value)
-
-        self.PANELS = {
-            panel.value: f'{SelectorFor.PANEL_DIV.value}:has-text("{panel.value}")'
-            for panel in PanelName
-        }
 
 
 # ===================================================
